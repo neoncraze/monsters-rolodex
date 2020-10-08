30a71c299226ab13a3756eaf3244a618e984e38d
Kayhwee <limkayhwee@hotmail.com>
2020-10-08 15:13:50 +0800

    Updates

---
+++ precache-manifest.67dd4268e70055f0fe8295883c04772e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff2166b564e21990594d0a644fb09cf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f2c89f85b60e72bc9527",
+    "url": "/monsters-rolodex/static/css/main.13459b23.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c89f85b60e72bc9527",
+    "url": "/monsters-rolodex/static/js/main.39e90eb2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
